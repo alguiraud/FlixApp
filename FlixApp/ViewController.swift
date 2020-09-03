d3e7b79e5df7be59bff35b59111e8ac368072abf
alguiraud <alg17d@my.fsu.edu>
2020-09-03 18:29:03 -0400

    initial

---
+++ FlixApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlixApp
+//
+//  Created by Andre Guiraud on 9/1/20.
+//  Copyright © 2020 Andre Guiraud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
