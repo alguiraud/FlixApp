826d4cbabcaf7f7e96035101dda2ef3a9a34b66b
alguiraud <alg17d@my.fsu.edu>
2020-09-13 00:28:15 -0400

    add collectionView and tabs

---
+++ FlixApp/MovieGridCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCollectionViewCell.swift
+//  FlixApp
+//
+//  Created by Andre Guiraud on 9/12/20.
+//  Copyright © 2020 Andre Guiraud. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
